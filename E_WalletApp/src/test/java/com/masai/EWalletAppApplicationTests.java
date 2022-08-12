a4d52020cad7b8258532e25c10d4ed2249e65d80
Bhasker23 <shrimalibhaskar2398@gmail.com>
2022-08-12 16:37:31 +0530

    E_WalletApp Created

---
+++ E_WalletApp/src/test/java/com/masai/EWalletAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EWalletAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
