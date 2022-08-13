c140718d34016860efde50f584b661291452e6ec
Bhasker23 <shrimalibhaskar2398@gmail.com>
2022-08-13 18:55:15 +0530

    Exception handling functionality added in Signup part

--- E_WalletApp/src/main/java/com/masai/servicesImpl/RegisterUserServiceImpl.java
+++ E_WalletApp/src/main/java/com/masai/servicesImpl/RegisterUserServiceImpl.java
@@ -55,44 +55,12 @@ public class RegisterUserServiceImpl implements RegisterUserServiceIntr {
 	    user.setCustomer(customer);
 	    
 		Wallet wallet = new Wallet();
-<<<<<<< HEAD
-		wallet.setBalance(108.1);
-//		wallet.setWalletId(1);
 		user.setWallet(wallet);
 		
-		Set<BankAccount> bankAccounts = new HashSet<>();
-		BankAccount bankAcc =new BankAccount(657845672, "EQIT23423", "Eqitas Finance Bank", 345680.3);
-		bankAccounts.add(bankAcc);
-		user.setBankAccounts(bankAccounts);//user will add their bank account after wallet account creation
-	    
-		Set<BeneficiaryDetails> beneficiaryDetails = new HashSet<>();
-	    //BeneficiaryDetails beneficiaryDetail = new BeneficiaryDetails("7492004935", "Satym Kumar Jha");
-	    BeneficiaryDetails beneficiaryDetail2 = new BeneficiaryDetails("8263039245", "Laxmi Didi");
-		//beneficiaryDetails.add(beneficiaryDetail2);
-		beneficiaryDetails.add(beneficiaryDetail2);
-		user.setBeneficiaryDetails(beneficiaryDetails);;//user will add their beneficiary after wallet account creation
-		
-		Set<Transaction> transactions = new HashSet<>();
-=======
-		user.setWallet(wallet);
-		
->>>>>>> 99813231d59360cc287e1d52e841e5773544b846
 		Transaction transaction = new Transaction();
 		BankAccount bankAcc = new BankAccount();
 	    BeneficiaryDetails beniBeneficiaryDetail = new BeneficiaryDetails();
 		
-<<<<<<< HEAD
-		transaction.setTransactionId(7774474);
-		transaction.setTransationType("Charus and ganja");
-		transaction.setTransactionAmount(3456.7);
-		transaction.setDescription("Sauk badee cheej hai... Tum kiya jano raesh babu...Kabhee haveli pe aao tab batayenge...Samjhe kee Nahee...?");
-		
-		transactions.add(transaction);
-		
-		user.setTransactions(transactions);//user will add their transactions after wallet account creation
-		 
-=======
->>>>>>> 99813231d59360cc287e1d52e841e5773544b846
 		customerDB.save(customer);
 		walletDB.save(wallet);
 		transactionDB.save(transaction);
