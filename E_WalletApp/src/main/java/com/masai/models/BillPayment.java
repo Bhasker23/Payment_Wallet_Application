86334ee1ba2964dcb21d6a6505bff730e7e95685
Aifanul Islam <sufyanislam00@gmail.com>
2022-08-14 22:56:42 +0530

    .

--- E_WalletApp/src/main/java/com/masai/models/BillPayment.java
+++ E_WalletApp/src/main/java/com/masai/models/BillPayment.java
@@ -1,14 +1,12 @@
 package com.masai.models;
 
-<<<<<<< HEAD
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
-=======
 import javax.persistence.CascadeType;
-import javax.persistence.Id;
 import javax.persistence.ManyToOne;
->>>>>>> e5f0c02c5f3b88a56c1b4a41b28995bde9827e42
+
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -27,12 +25,8 @@ public class BillPayment {
 	private Wallet wallet;
 	private Double billBmount;
 	private String billType;
-	
-<<<<<<< HEAD
-	
-	
-=======
+
 	@ManyToOne(cascade = CascadeType.ALL)
 	private UserAccountDetails user;
->>>>>>> e5f0c02c5f3b88a56c1b4a41b28995bde9827e42
+
 }
--- E_WalletApp/src/main/java/com/masai/models/Wallet.java
+++ E_WalletApp/src/main/java/com/masai/models/Wallet.java
@@ -5,13 +5,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-<<<<<<< HEAD
-=======
+
 import javax.persistence.ManyToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
->>>>>>> e5f0c02c5f3b88a56c1b4a41b28995bde9827e42
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
